2d36816b49c6c07b95a159c75b90e45a20b664a8
findraj <jankofindra@gmail.com>
2024-04-23 15:09:00 +0200

    change: differne naviagtion for mobile and laptop

---
+++ js/navigationButton.js
@@ -0,0 +1,11 @@
+function hideShow()
+{
+    if(document.getElementById('navbar-menu').style.display == 'block')
+    {
+        document.getElementById('navbar-menu').style.display = 'none';
+    }
+    else
+    {
+        document.getElementById('navbar-menu').style.display = 'block';
+    }
+};
