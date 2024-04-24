d19330d9cf14984b6f709cc0ed2c3ac4d960026b
findraj <jankofindra@gmail.com>
2024-04-24 22:42:16 +0200

    change: added timeline

---
+++ js/timeline.js
@@ -0,0 +1,57 @@
+function universityEnter() {
+    document.getElementById("2022").style.backgroundColor = "#b1b1b1";
+    document.getElementById("line4").style.backgroundColor = "#b1b1b1";
+    document.getElementById("2022").style.transition = "background-color 0.5s";
+    document.getElementById("line4").style.transition = "background-color 0.5s";
+}
+
+function universityLeave() {
+    document.getElementById("2022").style.backgroundColor = "#e1e1e1";
+    document.getElementById("line4").style.backgroundColor = "#e1e1e1";
+    document.getElementById("2022").style.transition = "background-color 0.5s";
+    document.getElementById("line4").style.transition = "background-color 0.5s";
+}
+
+function languageSchoolEnter() {
+    document.getElementById("2013").style.backgroundColor = "#b1b1b1";
+    document.getElementById("2021").style.backgroundColor = "#b1b1b1";
+    document.getElementById("line1").style.backgroundColor = "#b1b1b1";
+    document.getElementById("line2").style.backgroundColor = "#b1b1b1";
+    document.getElementById("2013").style.transition = "background-color 0.5s";
+    document.getElementById("2021").style.transition = "background-color 0.5s";
+    document.getElementById("line1").style.transition = "background-color 0.5s";
+    document.getElementById("line2").style.transition = "background-color 0.5s";
+}
+
+function languageSchoolLeave() {
+    document.getElementById("2013").style.backgroundColor = "#e1e1e1";
+    document.getElementById("2021").style.backgroundColor = "#e1e1e1";
+    document.getElementById("line1").style.backgroundColor = "#e1e1e1";
+    document.getElementById("line2").style.backgroundColor = "#e1e1e1";
+    document.getElementById("2013").style.transition = "background-color 0.5s";
+    document.getElementById("2021").style.transition = "background-color 0.5s";
+    document.getElementById("line1").style.transition = "background-color 0.5s";
+    document.getElementById("line2").style.transition = "background-color 0.5s";
+}
+
+function grammarSchoolEnter() {
+    document.getElementById("2018").style.backgroundColor = "#b1b1b1";
+    document.getElementById("2022").style.backgroundColor = "#b1b1b1";
+    document.getElementById("line2").style.backgroundColor = "#b1b1b1";
+    document.getElementById("line3").style.backgroundColor = "#b1b1b1";
+    document.getElementById("2018").style.transition = "background-color 0.5s";
+    document.getElementById("2022").style.transition = "background-color 0.5s";
+    document.getElementById("line2").style.transition = "background-color 0.5s";
+    document.getElementById("line3").style.transition = "background-color 0.5s";
+}
+
+function grammarSchoolLeave() {
+    document.getElementById("2018").style.backgroundColor = "#e1e1e1";
+    document.getElementById("2022").style.backgroundColor = "#e1e1e1";
+    document.getElementById("line2").style.backgroundColor = "#e1e1e1";
+    document.getElementById("line3").style.backgroundColor = "#e1e1e1";
+    document.getElementById("2018").style.transition = "background-color 0.5s";
+    document.getElementById("2022").style.transition = "background-color 0.5s";
+    document.getElementById("line2").style.transition = "background-color 0.5s";
+    document.getElementById("line3").style.transition = "background-color 0.5s";
+}
